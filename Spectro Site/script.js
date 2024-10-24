e17ad7d28ceac8bb80ec48e6194727e43249b025
cypripricheche <126987054+cypripricheche@users.noreply.github.com>
2024-10-24 21:20:51 +0200

    Add files via upload

---
+++ Spectro Site/script.js
@@ -0,0 +1,11 @@
+// ============================================================================================================================
+// MENU RESPONSIVE TOGGLE
+// ============================================================================================================================
+
+var menu = document.querySelector('.menu');
+var menu_toggle = document.querySelector('.menu_toggle');
+
+menu_toggle.onclick = function() {
+    menu_toggle.classList.toggle('active');
+    menu.classList.toggle('responsive');
+}
